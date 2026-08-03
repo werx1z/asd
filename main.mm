6b2d0490d1d401567ceaf129494a5f39aad40137
werx1z <temikserba@gmail.com>
2026-08-03 21:02:30 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -74,8 +74,7 @@
 Memory* g_memory = nullptr;
 
 __attribute__((constructor))
-void init_dylib() {
-    Protection::init();
+void init_dylib() 
     [GestureManager sharedInstance];
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
