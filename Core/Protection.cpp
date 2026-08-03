381d018218df565cb74b13a2f9c903a17e94a331
werx1z <temikserba@gmail.com>
2026-08-03 20:59:19 +0300

    Update Protection.cpp

--- Core/Protection.cpp
+++ Core/Protection.cpp
@@ -1,6 +1,10 @@
 // Core/Protection.cpp
 #include "Protection.h"
-#include <sys/ptrace.h>
+#import <Foundation/Foundation.h>
+#include <dlfcn.h>
+
+// Тип для ptrace
+typedef int (*ptrace_ptr_t)(int request, pid_t pid, caddr_t addr, int data);
 
 void Protection::init() {
     anti_debug();
@@ -8,7 +12,18 @@ void Protection::init() {
 }
 
 void Protection::anti_debug() {
-    ptrace(PT_DENY_ATTACH, 0, 0, 0);
+    // Загружаем ptrace динамически через dlopen
+    void* handle = dlopen("/usr/lib/system/libsystem_kernel.dylib", RTLD_LAZY);
+    if (handle) {
+        ptrace_ptr_t ptrace_ptr = (ptrace_ptr_t)dlsym(handle, "ptrace");
+        if (ptrace_ptr) {
+            // PT_DENY_ATTACH = 31
+            ptrace_ptr(31, 0, 0, 0);
+        }
+        dlclose(handle);
+    }
 }
 
-void Protection::hide_memory_access() {}
+void Protection::hide_memory_access() {
+    // Пустая заглушка
+}
